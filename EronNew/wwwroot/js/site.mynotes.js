bc7e627830e4c13d2fc7dcb150f86f0832ca8406
BILARIDIS2020\billi <billidis@outlook.com>
2021-03-07 22:46:53 +0200

    Fix Layout SideNavbar, Notes  , minor fixes search, details

--- EronNew/wwwroot/js/site.mynotes.js
+++ EronNew/wwwroot/js/site.mynotes.js
@@ -1,34 +1,26 @@
 ﻿(function () {
     function noteTemplate(note) {
         return `
-<li id="note_${note.Id}" class="d-flex flex-column mt-1 ml-1 mb-3">
-    <div class="chat-body white p-3 z-depth-1">
-        <div class="header">
-            <button type="button" class="deleteNote close" aria-label="Close" data-noteid="${note.Id}">
-                <span aria-hidden="true">×</span>
-            </button>
-            <strong class="primary-font">Σημείωση</strong>
-            <small class="pull-right text-muted"><i class="far fa-clock"></i> ${moment(note.CreatedAt).locale('el').format('LL')}</small>
-        </div>
-        <hr class="w-100">
-        <p class="mb-0">
-            ${note.Note}
-        </p>
-    </div>
-</li> `
+          <div id="note_${note.Id}" class="d-flex justify-content-start bg-light rounded-3 shadow-1 w-100">
+            <p class="p-3 col-11">
+                
+              ${note.Note}
+              <small class="float-end text-dark mt-4 mt-auto">${moment(note.CreatedAt).locale('el').format('LL')}</small>
+                
+            </p>
+            <button type="button" class="btn-close btn-sm mb-auto ms-auto  deleteNote" aria-label="Close" data-noteid="${note.Id}"></button>
+          </div>`
 
     }
 
  
     $(function () {
-        $(".chat-message").hide();
         $(".noteHeader").click(function () {
-            $(".chat-message").show();
             $('.friend-list').find('li').each(function (elem) {
                 $(this).removeClass("gradient-new-york").addClass("grey").addClass("lighten-3");
             });
             $(this).closest('li').removeClass("grey").removeClass("lighten-3").addClass("gradient-new-york");
-            $(".chatList").find('li').each(function () {
+            $(".chatList").find('div').each(function () {
                 $(this).remove();
             });
             let noteid = $(this).data("noteid");
